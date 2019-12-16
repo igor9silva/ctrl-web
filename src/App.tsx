4eec308e8fe7049b457927cc07273d5a14b6288b
Igor Silva <igor.silva@evolutionit.com.br>
2019-12-16 20:27:12 -0300

    adding table and user img

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
 import React from 'react';
 
 import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import FormControl from 'react-bootstrap/FormControl';
 
 const App: React.FC = () => {
 	return (
-		<Navbar bg="dark" variant="dark">
-			<Navbar.Brand href="#dashboard">CTRL</Navbar.Brand>
-			<Nav className="mr-auto">
-				<Nav.Link href="#dashboard">Dashboard</Nav.Link>
-				<Nav.Link href="#accounts">Accounts</Nav.Link>
-				<Nav.Link href="#transactions">Transactions</Nav.Link>
-				<Nav.Link href="#quotes">Quotes</Nav.Link>
-				<Nav.Link href="#settings">Settings</Nav.Link>
-			</Nav>
-			<Form inline>
-				<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-				<Button variant="outline-info">Search</Button>
-			</Form>
-		</Navbar>
+		<>
+			<Navbar bg="dark" variant="dark">
+				<Navbar.Brand href="#dashboard">CTRL</Navbar.Brand>
+				<Nav className="mr-auto">
+					<Nav.Link href="#dashboard">Dashboard</Nav.Link>
+					<Nav.Link href="#accounts">Accounts</Nav.Link>
+					<Nav.Link href="#transactions">Transactions</Nav.Link>
+					<Nav.Link href="#quotes">Quotes</Nav.Link>
+					<Nav.Link href="#settings">Settings</Nav.Link>
+				</Nav>
+				<Image src="/user.png" width={35} height={35} roundedCircle />
+			</Navbar>
+			<Table striped bordered hover variant="dark">
+				<thead>
+					<tr>
+					<th>#</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Username</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</Table>
+	  </>
 	);
 }
 
